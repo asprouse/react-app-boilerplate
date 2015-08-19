b16e6131ea03a1445bca312409f55734bf62865a
Andrew Sprouse <sprouse.a@gmail.com>
2015-08-19 12:33:32 -0400

    Consolidated client initter

--- app/client.js
+++ app/client.js
@@ -1,18 +1,68 @@
-import polyfill         from 'babel/polyfill';  // eslint-disable-line no-unused-vars
+import polyfill from 'babel/polyfill';  // eslint-disable-line no-unused-vars
 import styles from './styles/main.styl'; // eslint-disable-line no-unused-vars
 
-import config           from 'config/server';
-import initter          from './libs/initters/client';
-import setCookieDomain  from './libs/setCookieDomain';
-import routes           from './routes/routes';
-import reducers         from './reducers/reducers';
-import meta             from './layouts/meta';
+import React from 'react';
+import Router from 'react-router';
+import BrowserHistory from 'react-router/lib/BrowserHistory';
+import { combineReducers } from 'redux';
+import { applyMiddleware } from 'redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import middleware from 'redux-thunk';
+import NProgress from 'nprogress';
+
+import config from 'config/server';
+import Auth from './libs/Auth';
+import setTitle from './libs/setPageTitle';
+import analytics from './libs/analytics';
+import setCookieDomain from './libs/setCookieDomain';
+import routes from './routes/routes';
+import reducers from './reducers/reducers';
+import meta from './layouts/meta';
 
 
 const cookieDomain = setCookieDomain(document.location.hostname);
+const reducer = combineReducers(reducers);
+const store = applyMiddleware(middleware)(createStore)(reducer, window.__DATA__);
+const history = new BrowserHistory();
+const authAgent = new Auth(document, cookieDomain);
 
-const { googleAnalyticsId } = config;
+let initialRender = true;
 
-const params = { routes, reducers, meta, cookieDomain, googleAnalyticsId };
+if (config.googleAnalyticsId) {
+  analytics.init(config.googleAnalyticsId);
+}
 
-export default initter(params);
+function appComponent(Component, props) {
+  NProgress.configure({
+    showSpinner: false,
+    trickle    : true
+  });
+
+  if (props.route.name === 'app') {
+    NProgress.start();
+    analytics.sendPageview(props.location.pathname);
+  }
+
+  return (
+    <Component
+      store={store}
+      loader={NProgress}
+      authAgent={authAgent}
+      initialRender={initialRender}
+      setTitle={setTitle}
+      meta={meta}
+      {...props}
+      />
+  );
+}
+
+const AppContainer = (
+  <Provider store={store}>
+    {() => <Router history={history} children={routes({ store })} createElement={appComponent} />}
+  </Provider>
+);
+
+const appDOMNode = document.getElementById('app');
+
+React.render(AppContainer, appDOMNode, () => initialRender = false);
