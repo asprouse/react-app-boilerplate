9b11bf4ea5ee1a1778d942599435c758a013c363
Andrew Sprouse <sprouse.a@gmail.com>
2015-08-21 22:36:58 -0400

    added Link components

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,38 +1,25 @@
 import React from 'react';
-import { PropTypes as Type } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
-import * as AuthActions from '../actions/AuthActions';
-
-@connect(state => ({ auth: state.auth }))
 export default class Nav extends React.Component {
-  static propTypes = {
-    auth    : Type.object.isRequired,
-    dispatch: Type.func.isRequired
-  };
 
   render() {
-    const { auth, dispatch } = this.props;
-
     return (
       <nav className="top-bar" role="navigation">
         <div className="title-area">
           <h1>
-            <a href="/">
+            <Link to="/">
               <span className="circle"></span> Fair Tread
-            </a>
+            </Link>
           </h1>
         </div>
 
-
-
         <div className="right">
           <div className="login">
-            <a href="/login">Login</a>
+            <Link to="/login">Login</Link>
           </div>
           <div className="sign-up">
-            <a href="/signup"  className="round button">Sign Up - Join Today</a>
+            <Link to="/signup"  className="round button">Sign Up - Join Today</Link>
           </div>
         </div>
       </nav>
