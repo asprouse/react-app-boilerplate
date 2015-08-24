fa58cdcfae68da7c970471941462ffd0b9aed4e7
Andrew Sprouse <sprouse.a@gmail.com>
2015-08-24 01:45:25 -0400

    Updated Users tests to use mocha's built-in promise support

--- api/test/Users.test.js
+++ api/test/Users.test.js
@@ -16,53 +16,48 @@ describe('Users', () => {
   });
 
   describe('create', () => {
-    it('create a user', (done) => {
-      Users.create(user1).then((user) => {
+    it('create a user', () => {
+      return Users.create(user1).then((user) => {
         assert.isNotNull(user);
         assert.equal(user.email, email1);
         assert.notProperty(user, 'password');
         assert.property(redis.state, 'ft:user:' + user.id);
         assert.property(redis.state, 'ft:user-email:' + user.email);
-        done();
       });
     });
 
-    it('create fails when email is already in use', (done) => {
-      Users.create(user1)
+    it('create fails when email is already in use', () => {
+      return Users.create(user1)
         .then(() => {
           return Users.create({ email: email1, password: 'differentpassword' });
         })
         .then(() => {
           assert.fail();
-          done();
         }, (err) => {
           assert.isNotNull(err);
           assert.equal(err.cause, 'conflict');
-          done();
         });
     });
   });
 
   describe('authenticate', () => {
-    it('returns a user when auth is successful', (done) => {
-      Users.create(user1)
+    it('returns a user when auth is successful', () => {
+      return Users.create(user1)
         .then(() => {
           return Users.authenticate(email1, password1);
-        }, done)
+        })
         .then((user) => {
           assert.isNotNull(user);
-          done();
-        }, done);
+        });
     });
 
-    it('returns false when auth fails', (done) => {
-      Users.create(user1)
+    it('returns false when auth fails', () => {
+      return Users.create(user1)
         .then(() => {
           return Users.authenticate(email1, 'poopy');
         })
         .then((user) => {
           assert.isFalse(user);
-          done();
         });
     });
   });
