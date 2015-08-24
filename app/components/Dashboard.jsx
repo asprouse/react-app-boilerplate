907b0a0e1fb21566b5ab7634ccf41c984324ff08
Andrew Sprouse <sprouse.a@gmail.com>
2015-08-24 17:42:08 -0400

    Added padding to page-wrapper

--- app/components/Dashboard.jsx
+++ app/components/Dashboard.jsx
@@ -4,22 +4,24 @@ export default class Dashboard extends React.Component {
 
   render() {
     return (
-      <div id="dashboard">
-        <h2>Domains</h2>
-        <table>
-          <thead>
-          <tr>
-            <th>Domain</th>
-            <th>Origin</th>
-          </tr>
-          </thead>
-          <tbody>
+      <div className="dashboard">
+        <div className="dashboard__inner">
+          <h2>Domains</h2>
+          <table>
+            <thead>
+            <tr>
+              <th>Domain</th>
+              <th>Origin</th>
+            </tr>
+            </thead>
+            <tbody>
             <tr>
               <td>andrew.sprou.se</td>
               <td>andrew.sprou.se.s3-website-us-east-1.amazonaws.com</td>
             </tr>
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
